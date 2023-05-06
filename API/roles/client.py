7dd9a8002fd8668406ebe85604472d88c0beee1a
AmphiLink <59347293+AmphiLink@users.noreply.github.com>
2023-05-06 22:24:50 +0200

    début client

--- API/roles/client.py
+++ API/roles/client.py
@@ -1,2 +1,25 @@
+import os
+from time import sleep
+
+
 def main_client(cnx, Id_pers):
-    return 0
+    """
+    Cette fonction permet d'utiliser les différentes fonctionnnalités de l'application en tant que client.
+
+    Parameters:
+    -----------
+    cnx : mysql.connector.connection.MySQLConnection (Object)
+    Id_Pers : l'Id de la personne conectée (int)
+    """
+
+    choix = "basic"
+    os.system("cls")
+    while choix not in ("Reservé matériel", "Loué un emplacement", "Rejoindre une équipe", "Voir la liste des activités", "S'inscrire à une activité",  "profil", "exit", "1", "2", "3"):
+        choix = input(
+            "\nQue voulez vous faire ?\n (1: , 2: profil, 3: exit)\n ")
+        os.system("cls")
+
+    if choix == "exit" or choix == "3":
+        print("Vous avez quitté l'application !")
+        sleep(1)
+        exit()
